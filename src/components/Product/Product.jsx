b5adbd9d348f89b50a09354eb8c76e679f068b2f
shemuls <shemulpro@gmail.com>
2021-07-03 07:02:25 +0600

    Load dummy product from fakedata,, show single product by product component dynamically

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,5 +1,53 @@
-import React from "react";
+import {
+  faCoffee,
+  faShoppingCart,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
 
-export const Product = () => {
-  return <div></div>;
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const Product = ({ products }) => {
+  const { name, category, price, img } = products;
+  console.log(products);
+  const fullName = name;
+  const shortName = fullName.split(" ").splice(0, 5).join(" ");
+  const description = fullName.split(" ").splice(5).join(" ");
+  return (
+    <>
+      {/* product grid start */}
+      <div className="col-12 col-sm-8 col-md-6 col-lg-4 mb-3">
+        <div className="card">
+          <img
+            style={{ height: "250px", objectFit: "contain" }}
+            className="card-img p-3"
+            src={img}
+            alt="Vans"
+          />
+          <div className="card-img-overlay d-flex justify-content-end">
+            <a href="#d" className="card-link text-danger">
+              <FontAwesomeIcon icon={faHeart} />
+            </a>
+          </div>
+          <div className="card-body">
+            <h4 className="card-title">{shortName}</h4>
+            <h6 className="card-subtitle mb-2 text-muted">
+              Category: {category}
+            </h6>
+            <p className="card-text">{description}</p>
+            <div className="buy d-flex justify-content-between align-items-center">
+              <div className="price text-success">
+                <h5 className="mt-4">${price}</h5>
+              </div>
+              <a href="#d" className="btn btn-danger mt-3">
+                <FontAwesomeIcon className="mx-2" icon={faShoppingCart} />
+                Add to Cart
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* product grid end */}
+    </>
+  );
 };
--- src/containers/Shop/Shop.jsx
+++ src/containers/Shop/Shop.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { Product } from "./../../components/Product/Product";
+import fakeData from "./../../fakeData/index";
 
 export const Shop = () => {
-  return <div></div>;
+  const product12 = fakeData.splice(0, 12);
+  const [products, setproducts] = useState(product12);
+
+  return (
+    <div className="row my-5">
+      {products.map((singleProduct) => (
+        <Product key={singleProduct.key} products={singleProduct} />
+      ))}
+    </div>
+  );
 };
